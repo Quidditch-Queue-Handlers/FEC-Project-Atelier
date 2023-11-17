cf2d80daae525a2488611f28fc0528f96c2d6d96
Burhan Syed <busyed98@gmail.com>
2023-11-17 18:42:01 -0500

    display product info

--- client/src/components/ProductDetails/index.jsx
+++ client/src/components/ProductDetails/index.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductGallery from './ProductGallery';
 import ProductStyleSelector from './ProductStyleSelector';
 import ProductCartActions from './ProductCartActions';
+import ProductInfo from './ProductInfo';
 
 const ProductDetails = () => {
+
+  const [productInfo, setProductInfo] = useState();
+
   return (
     <div>
       <div className='pd-flex'>
-        <div className='pd-gallery-container'>
+        <div className='pd-wide-container'>
           <ProductGallery />
         </div>
         <div className='pd-flex-col pd-aside-container'>
           <div>
             reviews
           </div>
-          <h3>Product Category</h3>
-          <h1>Product Name</h1>
+          <h2>{productInfo?.category}</h2>
+          <h1>{productInfo?.name}</h1>
           <div>price</div>
           <ProductStyleSelector />
           <ProductCartActions />
         </div>
       </div>
-      <div>
-        Product Details & Info
-      </div>
+      <ProductInfo info={productInfo} />
     </div>
   );
 }
