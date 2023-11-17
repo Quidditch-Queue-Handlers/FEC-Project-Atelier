7bf135dec22e51e981604ea1d59f302eee1186c4
Burhan Syed <busyed98@gmail.com>
2023-11-16 20:35:06 -0500

    add todos in router

--- server/router.js
+++ server/router.js
@@ -1,6 +1,8 @@
 const controller = require('./controllers');
 const router = require('express').Router();
-//catch all for products
+
 router.get('/products*', controller.products.get);
+//TODO: Add /qa* controller
+//TODO: Add /reviews* controller
 
 module.exports = router;
