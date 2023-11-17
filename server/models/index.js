3af60903a48b29003a389de6560bd4838722862b
Burhan Syed <busyed98@gmail.com>
2023-11-16 21:04:25 -0500

    change environment variable name

--- server/models/index.js
+++ server/models/index.js
@@ -2,19 +2,19 @@ require('dotenv').config()
 const axios = require('axios');
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const API_ENDPOINT = process.env.API_ENDPOINT;
+const API_HOST = process.env.API_HOST;
 
 if (!GITHUB_TOKEN) {
   throw new Error('missing .env GITHUB_TOKEN');
 }
 
-if (!API_ENDPOINT) {
-  throw new Error('missing .env API_ENDPOINT');
+if (!API_HOST) {
+  throw new Error('missing .env API_HOST');
 }
 
 module.exports = {
   callAPI: function (req) {
-    return axios.get(`${API_ENDPOINT}${req.url}`, {
+    return axios.get(`${API_HOST}${req.url}`, {
       headers: {
         Authorization: GITHUB_TOKEN
       }
