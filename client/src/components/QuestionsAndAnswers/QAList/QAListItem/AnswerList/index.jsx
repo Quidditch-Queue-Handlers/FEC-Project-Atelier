a54b209daa41339d0175980974f24797588a6d69
Justin Cordova <justcord10@gmail.com>
2023-11-20 09:37:30 -0800

    added a note about the key props, and also added in another key prop to the answer list i am rendering as well

--- client/src/components/QuestionsAndAnswers/QAList/QAListItem/AnswerList/index.jsx
+++ client/src/components/QuestionsAndAnswers/QAList/QAListItem/AnswerList/index.jsx
@@ -4,12 +4,13 @@ import AnswerListItem from './AnswerListItem';
 const AnswerList = ({loadMoreAnswersClickHandler, displayedAnswerList, helpfulAnswerClickHandler, reportButtonClickHandler}) => {
 const [isScrollable, setIsScrollable] = React.useState(false); //will change this when i am actually passing in valid data
 
+//need to check answer.answer_id to ensure the prop name is correct.
   return (
     <div>
       <div>AnswerList</div>
       A:
       {displayedAnswerList?.map((answer) => (
-        <AnswerListItem answer={answer} helpfulAnswerClickHandler={helpfulAnswerClickHandler} reportButtonClickHandler={reportButtonClickHandler} />
+        <AnswerListItem answer={answer} key={answer?.answer_id} helpfulAnswerClickHandler={helpfulAnswerClickHandler} reportButtonClickHandler={reportButtonClickHandler} />
       ))}
       <button>Load More Answers</button>
     </div>
--- client/src/components/QuestionsAndAnswers/QAList/index.jsx
+++ client/src/components/QuestionsAndAnswers/QAList/index.jsx
@@ -5,11 +5,12 @@ const QAList = ({questionsList, displayedQuestionsList, loadMoreQuestionsClickHa
   const[displayCount, setDisplayCount] = React.useState(displayedQuestionsList?.length); //might not need this state
   const[isScrollable, setIsScrollable] = React.useState(false); //will beter determine this when i implement the scrollbar
 
+  //need to check question.question_id to ensure the prop name is correct.
   return (
     <div>
       <div>QAList</div>
-      {questionsList.map((question) => (
-        <QAListItem question={question} key={question?.product_id}/>
+      {questionsList?.map((question) => (
+        <QAListItem question={question} key={question?.question_id}/>
       ))}
       <button onClick={() => loadMoreQuestionsClickHandler()}>Load More Questions</button>
     </div>
