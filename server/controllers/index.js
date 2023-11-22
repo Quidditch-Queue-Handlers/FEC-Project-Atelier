666ebe546a0112a319d76983ad963ba29173eb8f
Burhan Syed <busyed98@gmail.com>
2023-11-22 16:20:10 -0500

    add cart api post route & simplify server

--- server/controllers/index.js
+++
@@ -1,3 +0,0 @@
-exports.products = require('./products');
-exports.qa = require('./qa');
-exports.reviews = require('./reviews');
--- server/index.js
+++ server/index.js
@@ -1,12 +1,55 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const router = require('./router');
+const axios = require('axios');
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const API_HOST_URI = process.env.API_HOST_URI;
+
+if (!GITHUB_TOKEN) {
+  throw new Error('missing .env GITHUB_TOKEN');
+}
+
+if (!API_HOST_URI) {
+  throw new Error('missing .env _URI');
+}
 
 const app = express();
 app.use(express.json());
-app.use('/', router);
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/*', (req, res) => {
+  axios
+    .get(`${API_HOST_URI}${req.url}`, {
+      headers: {
+        Authorization: GITHUB_TOKEN,
+      },
+    })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res
+        .status(err?.response?.status ?? 500)
+        .send(err?.response?.statusText ?? 'error');
+    });
+});
+app.post('/*', (req, res) => {
+  axios
+    .post(`${API_HOST_URI}${req.url}`, req?.body ?? {}, {
+      headers: {
+        Authorization: GITHUB_TOKEN,
+      },
+    })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res
+        .status(err?.response?.status ?? 500)
+        .send(err?.response?.statusText ?? 'error');
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 
