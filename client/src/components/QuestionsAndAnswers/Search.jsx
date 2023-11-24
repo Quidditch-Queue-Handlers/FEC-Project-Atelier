b6f4987f64168a2aeacded4b363d48246dffdde7
Justin Cordova <justcord10@gmail.com>
2023-11-23 22:14:57 -0800

    removed the commented out search button

--- client/src/components/QuestionsAndAnswers/Search.jsx
+++ client/src/components/QuestionsAndAnswers/Search.jsx
@@ -15,14 +15,11 @@ const searchInputChangeHandler = (e) => {
   console.log(e.target.value);
   setSearchInputText(e.target.value);
 }
-
-//will need to remove the button since the filter is kicked off automatically under different conditions, for now will keep there to test
   return (
     <div>
       <div>Search</div>
       <div>
         <input placeholder="Have a question? Search for answers..." onChange={(e) => searchInputChangeHandler(e)}></input>
-        {/* <button onClick={() => searchButtonClickHandler(searchInputText)}> Search</button> */}
       </div>
     </div>
   );
