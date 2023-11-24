8b9f740d6a303bf8bd6b9e8435957fe323c6760b
Justin Cordova <justcord10@gmail.com>
2023-11-23 19:28:52 -0800

    linked up the example data to my list to implement the search functionality in my component, also linked the Loadmore answers button to its click handler

--- client/src/components/QuestionsAndAnswers/index.jsx
+++ client/src/components/QuestionsAndAnswers/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import AddQuestion from './AddQuestion';
 import Search from './Search';
 import QAList from './QAList';
+import exampleData from '../../../examples/QA-examples/exampleQuestionCall.json';
 
 const QuestionsAndAnswers = ({product_id}) => {
   const [questionsList, setQuestionsList] = React.useState([]);
   const [displayedQuestionsList, setDisplayedQuestionsList] = React.useState([]);
   React.useEffect(() => {
     console.log('First QA Render');
+    console.log(exampleData);
+    setQuestionsList(exampleData.results);
+    setDisplayedQuestionsList(exampleData.results);
   }, []);
 const loadMoreQuestionsClickHandler = () => {
   console.log('clicked Load more Questions!');
@@ -23,8 +27,8 @@ const addQuestionClickHandler = () => {
     <div>
       <h2>Questions and Answers</h2>
       <div>
-        <Search searchButtonClickHandler={searchButtonClickHandler} />
-        <QAList questionsList={questionsList} displayedQuestionsList={displayedQuestionsList} loadMoreQuestionsClickHandler={loadMoreQuestionsClickHandler} />
+        {displayedQuestionsList.length !== 0 ? <Search searchButtonClickHandler={searchButtonClickHandler} /> : ""}
+        {displayedQuestionsList.length !== 0 ? <QAList questionsList={questionsList} displayedQuestionsList={displayedQuestionsList} loadMoreQuestionsClickHandler={loadMoreQuestionsClickHandler} /> : ""}
         <AddQuestion addQuestionClickHandler={addQuestionClickHandler} />
       </div>
     </div>
