9254f2ccea6490025c5191282678a1dd2b1f5a65
Derek Williams <dwilliams33dw@gmail.com>
2023-11-30 13:26:56 -0700

    made handlers and modal open state

--- client/src/components/RatingsAndReviews/WriteReview.jsx
+++ client/src/components/RatingsAndReviews/WriteReview.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const WriteReview = () => {
+const WriteReview = ({ productId, onReviewSubmit }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [overallRating, setOverallRating] = useState(0);
+  const [recommendation, setRecommendation] = useState('');
+
+  const handleRatingClick = (rating) => {
+    setOverallRating(rating);
+  };
+
+  const handleRecommendationChange = (value) => {
+    setRecommendation(value);
+  };
+
+  const handleReviewSubmit = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="rr-write-review">
-      <button>Add A Review +</button>
+      <button onClick={() => setModalOpen(true)}>Add A Review +</button>
+
+      {modalOpen && (
+        <div className="rr-write-review-modal">
+          <h2>Write Your Review</h2>
+          <h3>About the {productId}</h3>
+
+          <button onClick={handleReviewSubmit}>Submit Review</button>
+          <button onClick={() => setModalOpen(false)}>Close Modal</button>
+        </div>
+      )}
     </div>
   );
 }
