d454397e7d8328b0b3ae1ab69117145513d9fbc7
yuxinlu1 <huijialin0402@gmail.com>
2023-11-30 22:00:42 -0700

    version of best

--- client/src/components/RelatedItems/Outfits.jsx
+++ client/src/components/RelatedItems/Outfits.jsx
@@ -1,174 +1,170 @@
-import { useState, useEffect } from 'react';
-import Rating from '@mui/material/Rating';
-import { Virtual, Navigation, Pagination } from 'swiper/modules';
+import React, { useState, useEffect } from 'react';
 import ReviewStars from '../common/ReviewStars';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import {
   getDetailById,
-  getProductInfoById,
-  getReviewsMeta, 
+  getReviewsMeta,
 } from './api/product-api';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
-const Outfits = ({ productId }) => { 
-  const [swiperRef, setSwiperRef] = useState(null);
+const Outfits = ({ productId }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const [slides, setSlides] = useState([]);
   const [ratings, setRatings] = useState({});
 
   useEffect(() => {
-    if (localStorage.slide) setSlides(JSON.parse(localStorage.slide));
+    const storedSlides = localStorage.getItem('slide');
+    if (storedSlides) {
+      setSlides(JSON.parse(storedSlides));
+    }
   }, []);
 
   const handleRemoveOutfit = (id) => {
-    const newSlides = slides.filter((item) => item.id !== id);
+    const newSlides = slides.filter(slide => slide.id !== id);
     setSlides(newSlides);
-    localStorage.setItem('slide', JSON.stringify(newSlides)); 
+    localStorage.setItem('slide', JSON.stringify(newSlides));
   };
 
   const handleAddOutfit = async () => {
+    if (slides.some(slide => slide.id === productId)) return;
+
     try {
-      // Check is already added or not...
-      const isAlreadyAdded = slides.find(slide => slide.id === productId);
-      if (isAlreadyAdded) {
-        console.log('Outfit already added.');
-        return; 
-      }
-  
       const [detailResponse, reviewsMetaResponse] = await Promise.all([
         getDetailById(productId),
         getReviewsMeta(productId),
       ]);
-      const goodsDetail = detailResponse.data.results[0];
-      
-      const ratings = reviewsMetaResponse?.data?.ratings ?? {};
-      let count = 0;
-      let total = 0;
-      Object.entries(ratings).forEach((entry) => {
-        count += +(entry?.[1] ?? 0);
-        total += +(entry?.[0] ?? 0) * +(entry?.[1] ?? 0);
-      });
-      let averageRate = total / count;
-    
+
       const newSlideContent = {
         id: productId,
-        ...goodsDetail,
-        averageRate,
+        ...detailResponse.data.results[0],
+        averageRate: calculateAverageRate(reviewsMetaResponse?.data?.ratings),
       };
-    
-      const updatedSlides = [...slides, newSlideContent];
-      setSlides(updatedSlides);
-      setRatings({...ratings, [productId]: averageRate});
-    
-      localStorage.setItem('slide', JSON.stringify(updatedSlides));
+
+      setSlides(prevSlides => [...prevSlides, newSlideContent]);
+      localStorage.setItem('slide', JSON.stringify([...slides, newSlideContent]));
     } catch (error) {
       console.error('Error adding outfit: ', error);
     }
   };
 
-  const handleRatingChange = (id, newRating) => {
-    setRatings({...ratings, [id]: newRating});
+  const calculateAverageRate = (ratings) => {
+    let total = 0, count = 0;
+    for (const [rate, number] of Object.entries(ratings || {})) {
+      total += rate * number;
+      count += number;
+    }
+    return count > 0 ? total / count : 0;
   };
+
+  const handleNext = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentSlide((prevSlide) => prevSlide === 0 ? slides.length - 1 : prevSlide - 1);
+  };
+
+  // 样式定义，保持与 RelatedProducts 组件一致
+  const carouselContainerStyle = {
+    position: 'relative',
+    width: '100%',
+    overflow: 'hidden',
+  };
+  
+  const carouselWrapperStyle = {
+    display: 'flex',
+    transform: `translateX(-${currentSlide * 30}%)`, 
+    transition: 'transform 0.3s ease-in-out',
+  };
+  
+  const carouselSlideStyle = {
+    flex: '0 0 30%',
+    opacity: 1,
+    transition: 'opacity 0.3s ease-in-out',
+    margin: '0 5px',
+  };
+
+  const imageStyle = {
+    height: '250px',
+    backgroundColor: 'grey',
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+  const slideImageStyle = {
+    height: '380px',
+    width: '250px',
+    border: 'solid',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative', 
+    }
+  const firststyle = {
+    height: '380px',
+    width: '250px',
+    border: 'solid',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative', 
+    backgroundColor: '#e9e9e9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  const carouselControlStyle = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    backgroundColor: '#fff',
+    border: 'none',
+    cursor: 'pointer',
+  };
+
+  const carouselPrevStyle = {
+    ...carouselControlStyle,
+    left: '10px',
+  };
+
+  const carouselNextStyle = {
+    ...carouselControlStyle,
+    right: '10px',
+  };
+  const OutfitStyles = {
+    maxWidth: '80%', 
+    margin: '0 auto', 
+  };
+
   return (
-    <>
+    <div style={OutfitStyles}>
       <h4>YOUR OUTFIT</h4>
-      <Swiper
-        modules={[Virtual, Navigation]}
-        onSwiper={setSwiperRef}
-        slidesPerView={3}
-        spaceBetween={50}
-        pagination={{
-          type: 'fraction',
-        }}
-        navigation={true}
-        virtual
-      >
-        <SwiperSlide>
-          <div
-            style={{
-              cursor: 'pointer',
-              height: '380px',
-              width: '250px',
-              border: 'solid',
-              backgroundColor: '#e9e9e9',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            onClick={handleAddOutfit}
-          >
-            <span style={{ fontSize: '200px', color: 'grey' }}>&#43;</span>
+      <div style={carouselContainerStyle}>
+        <div style={carouselWrapperStyle}>
+          <div style={carouselSlideStyle}>
+            <div style={firststyle}>
+              <span style={{ cursor: 'pointer', fontSize: '200px', color: 'grey' }} onClick={handleAddOutfit}>&#43;</span>
+            </div>
           </div>
-        </SwiperSlide>
-
-        {slides.map((slideContent, index) => (
-          <SwiperSlide key={slideContent.id} virtualIndex={index}>
-            <div
-              style={{
-                height: '380px',
-                width: '250px',
-                border: 'solid',
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <div
-                style={{
-                  height: '250px',
-                  backgroundColor: '#e9e9e9',
-                  position: 'relative',
-                  overflow: 'hidden',
-                  backgroundImage: `url(${slideContent.photos[0].url})`,
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <span
-
-                  style={{
-                    position: 'absolute',
-                    top: '10px',
-                    right: '10px',
-                    cursor: 'pointer',
-                    color: 'white',
-                  }}
-                  onClick={() => handleRemoveOutfit(slideContent.id)}>
+          {slides.map((slide, index) => (
+            <div key={slide.id} style={carouselSlideStyle}>
+              <div style={slideImageStyle}>
+                <div style={{ ...imageStyle, backgroundImage: `url(${slide.photos[0].url})` }}>
+                  <span style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer', color: 'white' }}
+                      onClick={() => handleRemoveOutfit(slide.id)}>
                     &#9746;
-               </span>
-              </div>
-              <div
-                style={{
-                  flex: 1,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-evenly',
-                  alignItems: 'flex-start',
-                  padding: '0 10px',
-                }}
-              >
-                <h2 style={{ fontWeight: 400, fontSize: '1rem' }}>
-                  categroy
-                </h2>
-                <span style={{ fontWeight: 800 }}>
-                  {slideContent.name.trim()}
-                </span>
-                <h3 style={{ fontSize: '1rem' }}>
-                  ${slideContent.original_price}
-                </h3>
-                <ReviewStars
-                  rating={ratings[slideContent.id] || 0}
-                  ratingId={slideContent.id} 
-                  onRatingChange={(newRating) => {
-                  handleRatingChange(slideContent.id, newRating);
-                  }}
-                />
+                  </span>
+                </div>
+                <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'flex-start', padding: '0 10px' }}>
+                  <span>{slide.category}</span>
+                  <span>{slide.name}</span>
+                  <span>${slide.original_price}</span>
+                  <ReviewStars rating={ratings[slide.id] || slide.averageRate} ratingId={`review_${slide.id}`} />
+                </div>
               </div>
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+          ))}
+        </div>
+        <button style={carouselNextStyle} onClick={handleNext}>&gt;</button>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/RelatedItems/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedItems/RelatedProducts/RelatedProducts.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Modal } from 'antd';
 import { StarOutlined } from '@ant-design/icons';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Virtual, Navigation, Pagination } from 'swiper/modules';
-import FeatureComparison from './FeatureComparison'; 
 import ReviewStars from '../../common/ReviewStars';
+import FeatureComparison from './FeatureComparison'; 
 import {
   getRelatedIds,
   getDetailById,
   getProductInfoById,
   getReviewsMeta,
 } from '../api/product-api';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
-const RelatedProducts = ( { productId, setProductId } ) => {
+const RelatedProducts = ({ productId, setProductId }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [swiperRef, setSwiperRef] = useState(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const [slides, setSlides] = useState([]);
+  const [compareProduct, setCompareProduct] = useState(null);
   const [fixedCompareInfo, setFixedCompareInfo] = useState([]);
   const [features, setFeatures] = useState({});
+
 
   useEffect(() => {
     getRelatedIdsApi();
@@ -76,19 +72,20 @@ const RelatedProducts = ( { productId, setProductId } ) => {
       const compareFeatures = compareProductInfo.data.features;
       const featureSet = new Set([...mainFeatures.map(f => f.feature), ...compareFeatures.map(f => f.feature)]);
       const featureComparison = {};
-
+  
       featureSet.forEach(feature => {
         const mainFeature = mainFeatures.find(f => f.feature === feature);
         const compareFeature = compareFeatures.find(f => f.feature === feature);
-
+  
         featureComparison[feature] = [
           mainFeature ? mainFeature.value : 'N/A',
           compareFeature ? compareFeature.value : 'N/A',
         ];
       });
-
+  
       setFeatures(featureComparison);
       setIsModalOpen(true);
+      setCompareProduct(compareProductInfo.data);
     } catch (err) {
       console.error('Error in handleCompare:', err);
     }
@@ -104,93 +101,147 @@ const RelatedProducts = ( { productId, setProductId } ) => {
   const onProductClick = (relatedProductId) => {
     setProductId(relatedProductId);
   };
+  // Carousel style
+  const carouselContainerStyle = {
+    position: 'relative',
+    width: '100%',
+    overflow: 'hidden',
+  };
+  
+  const carouselWrapperStyle = {
+    display: 'flex',
+    transform: `translateX(-${currentSlide * 30}%)`, 
+    transition: 'transform 0.3s ease-in-out',
+  };
+  
+  const carouselSlideStyle = {
+    flex: '0 0 30%',
+    opacity: 1,
+    transition: 'opacity 0.3s ease-in-out',
+    margin: '0 5px',
+  };
+
+  const carouselSlideActiveStyle = {
+    ...carouselSlideStyle,
+    opacity: 1,
+  };
+
+  const carouselControlStyle = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    backgroundColor: '#fff',
+    border: 'none',
+    cursor: 'pointer',
+  };
+
+  const carouselPrevStyle = {
+    ...carouselControlStyle,
+    left: '10px',
+  };
+
+  const carouselNextStyle = {
+    ...carouselControlStyle,
+    right: '10px',
+  };
+  const handlePrev = () => {
+    setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : Math.ceil(slides.length / 3.5) - 1);
+  };
+  
+  const handleNext = () => {
+      setCurrentSlide(currentSlide < Math.ceil(slides.length / 3.5) - 1 ? currentSlide + 1 : 0);
+  };
+  
+  const modalStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'white',
+    padding: '20px',
+    zIndex: 1000,
+    display: isModalOpen ? 'block' : 'none',
+  };
+
+  const backdropStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 999,
+    display: isModalOpen ? 'block' : 'none',
+  };
+  const slideImageStyle = {
+    height: '380px',
+    width: '250px',
+    border: 'solid',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative', 
+    }
+  const imageStyle = {
+    height: '250px',
+    backgroundColor: 'grey',
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+  const starStyles = {
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+    cursor: 'pointer',
+    color: 'green',   
+    zIndex: 10,
+  }
+  const relatedProductsContainerStyle = {
+    maxWidth: '80%', 
+    margin: '0 auto', 
+  };
 
 
   return (
-    <>
+    <div style={relatedProductsContainerStyle}>
       <h4>RELATED PRODUCTS</h4>
-      <Swiper
-        modules={[Virtual, Navigation, Pagination]}
-        onSwiper={setSwiperRef}
-        slidesPerView={3}
-        spaceBetween={50}
-        pagination={{
-        type: 'fraction',
-        }}
-       navigation={true}
-       virtual
-      >
-  {slides.map((slideContent, index) => (
-    <SwiperSlide key={slideContent.id} onClick={() => onProductClick(slideContent.id)} virtualIndex={index}>
-      <div
-        style={{
-          height: '380px',
-          width: '250px',
-          border: 'solid',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <div
-          style={{
-            height: '250px',
-            backgroundColor: 'grey',
-            position: 'relative',
-            overflow: 'hidden',
-            backgroundImage: `url(${slideContent.photos[0].url})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
-        >
-          <StarOutlined
-            style={{
-              position: 'absolute',
-              top: '10px',
-              right: '10px',
-              cursor: 'pointer',
-              color: 'white',
-            }}
-            onClick={() => handleCompare(slideContent.id)}
-          />
+      <div style={carouselContainerStyle}>
+        <div style={carouselWrapperStyle}>
+          {slides.map((slideContent, index) => (
+            <div
+              key={slideContent.id}
+              style={index === currentSlide ? carouselSlideActiveStyle : carouselSlideStyle}
+              onClick={() => onProductClick(slideContent.id)}
+              data-testid={`product-${slideContent.id}`}
+            >
+              <div style={slideImageStyle}>
+                <img src={slideContent.photos[0].url} style={imageStyle} alt="Product Image" />
+                <StarOutlined style={starStyles} onClick={() => handleCompare(slideContent.id)} data-testid={`compare-button-${slideContent.id}`} />
+                <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'flex-start', padding: '0 10px' }}>
+                  <span style={{ fontWeight: 400, fontSize: '15px' }}>{slideContent.category}</span>
+                  <span style={{ fontWeight: 800 }}>{slideContent.name.trim()}</span>
+                  <span style={{ fontSize: '12px' }}>${slideContent.original_price}</span>
+                  <ReviewStars rating={slideContent.averageRate} ratingId={`review_${slideContent.id}`} />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        <div
-          style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-            alignItems: 'flex-start',
-            padding: '0 10px',
-          }}
-        >
-          <span style={{ fontWeight: 400, fontSize: '15px' }}>
-          {slideContent.category}
-          </span>
-          <span style={{ fontWeight: 800 }}>
-            {slideContent.name.trim()}
-          </span>
-          <span style={{ fontSize: '12px' }}>
-            ${slideContent.original_price}
-          </span>
-          <ReviewStars
-            rating={slideContent.averageRate}
-            ratingId={`review_${slideContent.id}`}
-          />
-        </div>
-      </div>
-    </SwiperSlide>
-  ))}
-</Swiper>
+        {currentSlide !== 0 && (
+        <button style={carouselPrevStyle} onClick={handlePrev}>&lt;</button>
+      )}
 
-      <Modal
-        title="COMPARING"
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
+        <button style={carouselNextStyle} onClick={handleNext}>&gt;</button>
+      </div>
+
+      <div style={backdropStyle} onClick={handleCompare}></div>
+      <div style={modalStyle}>
+        {fixedCompareInfo.length > 0 && (
+         <>
           <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-              <h4>{fixedCompareInfo[0]?.name}</h4>
-              <h4>{fixedCompareInfo[1]?.name}</h4>
+          <h4>{fixedCompareInfo[0]?.name}</h4>
+          <h4>{fixedCompareInfo[1]?.name}</h4>
           </div>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
             <span style={{ width: '33%', textAlign: 'center' }}>{fixedCompareInfo[0]?.category}</span>
@@ -205,8 +256,13 @@ const RelatedProducts = ( { productId, setProductId } ) => {
           {Object.entries(features).map(([feature, values]) => (
             <FeatureComparison key={feature} feature={feature} values={values} />
           ))}
-      </Modal>
-    </>
+        </>
+        )}
+       <button onClick={handleCancel}>Close</button>
+      </div>
+    </div>
   );
 };
+
+
 export default RelatedProducts;
